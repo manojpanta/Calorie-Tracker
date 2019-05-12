11615970adf517bf4ab9528b1e2de0cb86fb4f2b
manojpanta <manojpanta95@gmail.com>
2019-05-12 13:58:49 -0600

    Add basic react app setup , sign in and sign up components
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

---
+++ src/pages/SignUpForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class SignUpForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: '',
+            password_confirmation: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let target = e.target;
+        let value = target.value;
+        let name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      console.log(JSON.stringify(this.state));
+      const url = "https://quantified-self1811.herokuapp.com/api/v1/users";
+      if (this.state.password=== this.state.password_confirmation) {
+        fetch(url,
+      {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          body: JSON.stringify(this.state)
+      })
+      .then(response => response.json())
+      .then(result => console.log(result.body))
+
+      }else {
+      }
+    }
+
+    render() {
+        return (
+        <div className="FormCenter">
+            <form onSubmit={this.handleSubmit} className="FormFields">
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="name">Full Name</label>
+                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="password">Password</label>
+                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="passwordConfirmation">Password Confirmation</label>
+                <input type="password" id="password-confirmation" className="FormField__Input" placeholder="Confirm your password" name="password_confirmation" value={this.state.password_confirmation} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+              </div>
+
+              <div className="FormField">
+                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/sign-in" className="FormField__Link">Im already member</Link>
+              </div>
+            </form>
+          </div>
+        );
+    }
+}
+
+export default SignUpForm;
