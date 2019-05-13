568e1830a6bbab13a73c48c48b0749798967af5e
Scott Thomas <smthom05@gmail.com>
2019-05-13 17:52:12 -0600

    Add props to user profile. Need to pass more information from the QS service to our front end
    Co-authored-by: Manoj Panta <34468256+manojpanta@users.noreply.github.com>

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import App from '../App'
+
 
 class Users extends Component {
   constructor(props) {
     super();
+
+    this.state = {
+      email: props.location.email,
+      password: props.location.password
+    }
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="FormCenter">
-        <h1>Hello World!!</h1>
+        <div className='WelcomeBanner'>
+          <h1> Welcome {this.state.email} </h1>
+          <h1> Password:{this.state.password} </h1>
+        </div>
       </div>
     );
   }
