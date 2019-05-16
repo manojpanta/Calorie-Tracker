61dc86508687484e75e43cc8dd3ab96c9a76ee0d
manojpanta <manojpanta95@gmail.com>
2019-05-15 22:35:27 -0600

    fixed checkbox that lets us select mealtype and clicking submit button from recipe section passes params to handlesubmit method

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -7,42 +7,43 @@ class Recipes extends Component {
   constructor() {
     super();
     this.state = {
-      mealType: '',
-      foodName: ''
+      mealType: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
+  async handleChange(e) {
     let target = e.target;
     let value = target.value;
-    this.setState({
+    await this.setState({
       mealType: value
     });
   }
 
-  handleSubmit(e) {
+  handleSubmit(foodName, calories, e) {
     e.preventDefault();
     const url = "https://quantified-self1811.herokuapp.com/api/v1/foods";
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    let body = {
-      "name": this.state.foodName,
-      "calories": this.state.foodCalories
-    };
-
+    let body = {"food": {"name": foodName, "calories": calories }}
+    console.log(body)
     if (this.state.mealType !== '') {
       fetch(proxyurl + url,
         {
           headers: {
+            'Origin': 'X-Requested-With',
+            'cookie': 'cookie2',
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
           method: "POST",
           body: JSON.stringify(body)
       })
-      .then(response => response.json())
       .then(result => {
+        console.log(result)
+      })
+      .catch(error=> {
+        console.log(error)
       })
     } else {
       alert("Please Choose Your Meal Type")
@@ -65,7 +66,8 @@ class Recipes extends Component {
                     <h4> Cook Time: {recipes[key].cook_time}</h4>
                     <h4> Calories: {Math.round(recipes[key].calories)}</h4>
                     <h4> Ingredients :{recipes[key].ingredients}</h4></center>
-                    <form onSubmit={this.handleSubmit} className="FormFields">
+
+                    <form onSubmit={this.handleSubmit.bind(this, recipes[key].label, recipes[key].calories)} className="FormFields">
                       <label className="FormField__CheckboxLabel">
                         <input className="FormField__Checkbox" type="checkbox" name="mealType" value="Breakfast" onChange={this.handleChange} />Breakfast
                         <input className="FormField__Checkbox" type="checkbox" name="mealType" value="Lunch" onChange={this.handleChange} />Lunch
@@ -74,6 +76,7 @@ class Recipes extends Component {
                       </label>
                       <center><button>Cook It!</button></center>
                     </form>
+
                 </div>
               </>
               )
